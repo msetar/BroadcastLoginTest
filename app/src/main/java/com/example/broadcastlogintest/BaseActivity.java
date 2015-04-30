fca2c26d56530acab0ccd629f52c588575d9080d
msetar <salyer0301@163.com>
2015-04-30 10:07:18 +0800

    BaseActivity Test

---
+++ app/src/main/java/com/example/broadcastlogintest/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.example.broadcastlogintest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lifb on 2015/4/30.
+ */
+public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
